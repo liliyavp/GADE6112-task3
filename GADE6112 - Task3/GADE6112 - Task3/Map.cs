a9325fe5a8cb328f9cb55713e42efe974747dbe6
liliyavp <55442399+liliyavp@users.noreply.github.com>
2019-10-15 14:04:01 +0200

    Update Map.cs

--- GADE6112 - Task3/GADE6112 - Task3/Map.cs
+++ GADE6112 - Task3/GADE6112 - Task3/Map.cs
@@ -10,26 +10,30 @@ namespace GADE6112___Task3
     {
         public readonly int width = 20;
         public readonly int height = 20;
+        //can only assign readonly here or in constructor
 
         string[,] map;
 
         public Map(int width, int height) {
-
+            //NOTE: no more numUnits, numBuildings
+            //separated maps to display, and managers into separate class
             this.width = width;
             this.height = height;
             map = new string[width, height];
+            //sets width and height and creates map string
         }
 
         public void UpdateMap(UnitAndBuildingManager manager)
+            //passes manager class
         {
-            for (int y = 0; y < height; y++) {
+            for (int y = 0; y < height; y++) { //height and width instead of y and x in all code
                 for (int x = 0; x < width; x++) {
                     map[x, y] = "   ";
                 }
             }
 
             foreach (Unit unit in manager.Units) {
-                if (unit.IsVisible) {
+                if (unit.IsVisible) { //if visible, draw it. After a while, dead things disappear
                     map[unit.X, unit.Y] = unit.Symbol + "|" + unit.Faction[0];
                 }
             }
@@ -53,6 +57,8 @@ namespace GADE6112___Task3
                 mapString += "\n";
             }
             return mapString;
+            //puts str arr into single string then returns to display later
         }
     }
 }
+//purpose of entire code just to display
